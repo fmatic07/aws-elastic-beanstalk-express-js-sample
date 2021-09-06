47d54aa2e5044fcf3ac5dcd6164439ad78bf2cc9
Franque <franquebmatic@gmail.com>
2021-09-06 08:12:27 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Franque the Great!'));
+app.get('/', (req, res) => res.send('Hello Franque the Greatest!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
